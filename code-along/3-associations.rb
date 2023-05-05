4c518bd53fb22523a365e48d6f43fa25679eccde
Mustapha Baryoh <mustapha.baryoh@kellogg.northwestern.edu>
2023-05-05 04:17:50 +0000

    commit6

--- code-along/3-associations.rb
+++ code-along/3-associations.rb
@@ -11,7 +11,29 @@ Contact.destroy_all
 # - Insert and read contact data for companies in the database
 
 # 1. insert new rows in the contacts table with relationship to a company
+apple = Company.find_by({ "name" => "Apple"})
+new_contact = Contact.new
+new_contact["first_name"] = "Tim"
+new_contact["last_name"] = "Cook"
+new_contact["email"] = "tim@apple.com"
+new_contact["company_id"] = apple["id"]
+new_contact.save
+
+
+new_contact = Contact.new
+new_contact["first_name"] = "Craig"
+new_contact["last_name"] = "Federighi"
+new_contact["email"] = "craig@apple.com"
+new_contact["company_id"] = apple["id"]
+new_contact.save
 
 # 2. How many contacts work at Apple?
+apple_contacts = Contact.where({ "company_id" => apple["id"] })
+puts "Contacts at Apple: #{apple_contacts.count}"
 
 # 3. What is the full name of each contact who works at Apple?
+for contact in apple_contacts
+    first_name = contact["first_name"]
+    last_name = contact["last_name"]
+    puts "#{first_name} #{last_name}"
+end
--- labs/3-associations.rb
+++ labs/3-associations.rb
@@ -16,6 +16,14 @@ Activity.destroy_all
 
 # 1. insert 3 rows in the activities table with relationships to
 # a single salesperson and 2 different contacts
+ben = Salesperson.find_by({"first_name" => "Benjamin", "last_name" => "Block" })
+tim = Contact.find_by({ "first_name" => "Tim", "last_name" => "Cook"})
+
+activity = Activity.new
+activity["salesperson_id"] = ben["id"]
+activity["contact_id"] = tim["id"]
+activity["note"] = "had tacos"
+activity.save
 
 # 2. Display all the activities between the salesperson used above
 # and one of the contacts (sample output below):
