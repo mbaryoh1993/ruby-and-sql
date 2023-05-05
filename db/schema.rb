7b8242226689d0ec55d39c7b9922d7cd928306a9
Mustapha Baryoh <mustapha.baryoh@kellogg.northwestern.edu>
2023-05-05 01:52:15 +0000

    commit3

--- db/schema.rb
+++ db/schema.rb
@@ -10,7 +10,7 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema[7.0].define(version: 2023_05_05_012509) do
+ActiveRecord::Schema[7.0].define(version: 2023_05_05_015101) do
   create_table "companies", force: :cascade do |t|
     t.string "name"
     t.string "city"
@@ -25,6 +25,14 @@ ActiveRecord::Schema[7.0].define(version: 2023_05_05_012509) do
     t.string "last_name"
     t.string "email"
     t.integer "company_id"
+    t.datetime "created_at", null: false
+    t.datetime "updated_at", null: false
+  end
+
+  create_table "salespeople", force: :cascade do |t|
+    t.string "first_name"
+    t.string "last_name"
+    t.string "email"
     t.datetime "created_at", null: false
     t.datetime "updated_at", null: false
   end
